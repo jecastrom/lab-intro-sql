485b89032d219278f07355e38c7a72c769057943
jecastrom <jcastro.de@outlook.com>
2021-12-11 19:32:38 +0100

    commit

---
+++ .github/Lab SQL Intro.sql
@@ -0,0 +1,19 @@
+select * from 
+actor, film, customer;
+
+select title
+from film;
+
+select distinct name as language
+from language;
+
+select count(*) 
+from store;
+
+select count(*)
+from staff;
+
+select first_name
+from staff;
+
+
